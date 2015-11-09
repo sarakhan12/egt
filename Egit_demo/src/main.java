1db31740b5a1635daf5c6c3a442e5f24cf852d3c
FA12-BSE-253 <FA12-BSE-253@LAB7C-02.cns.ciit>
2015-11-09 06:30:38 +0500

    commit1

--- Egit_demo/src/main.java
+++ Egit_demo/src/main.java
@@ -4,6 +4,8 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Lets do something with GIT");
+		System.out.println("changed");
+		System.out.println("sunday");
 
 	}
 
